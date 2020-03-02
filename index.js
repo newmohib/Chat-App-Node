fbe35a0c12e04fd962b1d2f5e1ca83366fdb32cf
unknown <newmohib@gmail.com>
2020-03-02 21:20:57 +0600

    added broadcast

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
 // io.on('connection', function(socket){
@@ -13,12 +13,23 @@ app.get('/', function(req, res){
 //     });
 //   });
 
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-      console.log('message: ' + msg);
-    });
-  });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+// io.on('connection', function(socket){
+//     socket.on('chat message', function(msg){
+//       console.log('message: ' + msg);
+//     });
+//   });
+
+io.on('connection', function (socket) {
+    socket.broadcast.emit('hi');
+});
+
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
