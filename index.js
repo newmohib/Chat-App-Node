7e88f5323186d0d85af9ab03147c51f35c6ebfac
unknown <newmohib@gmail.com>
2020-03-04 20:32:00 +0600

    broadcust added

--- index.js
+++ index.js
@@ -6,32 +6,15 @@ app.get('/', function(req, res) {
    res.sendfile('index.html');
 });
 
-//Whenever someone connects this gets executed
+var clients = 0;
 io.on('connection', function(socket) {
-   console.log('A user connected');
-
-   // Send a message after a timeout of 4 seconds defalult events
-    // setTimeout(function() {
-    // socket.send('Sent a message 4seconds after connection!');
-    // }, 4000);
-
-
-  //Send a message to client when custom events 
-//    setTimeout(function() {
-//       //Sending an object when emmiting an event
-//       socket.emit('testerEvent', { description: 'A custom event named testerEvent!'});
-//    }, 4000);
-
-io.on('connection', function(socket) {
-    socket.on('clientEvent', function(data) {
-       console.log(data);
-    });
- });
- 
- 
-   //Whenever someone disconnects this piece of code executed
+   clients++;
+   console.log("clients 1 :",clients);
+   io.sockets.emit('broadcast',{ description: clients + ' clients connected!'});
+   
    socket.on('disconnect', function () {
-      console.log('A user disconnected');
+      clients--;
+      io.sockets.emit('broadcast',{ description: clients + ' clients connected!'});
    });
 });
 
