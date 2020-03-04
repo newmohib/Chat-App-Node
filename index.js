a7e219f855d4f38680ef5866f61c710e8974f716
unknown <newmohib@gmail.com>
2020-03-04 19:50:34 +0600

    client to server send message

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 var app = require('express')();
-var http = require('http').createServer(app);
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+   res.sendfile('index.html');
 });
 
-// io.on('connection', function(socket){
-//     console.log('a user connected');
-//     socket.on('disconnect', function(){
-//       console.log('user disconnected');
-//     });
-//   });
+//Whenever someone connects this gets executed
+io.on('connection', function(socket) {
+   console.log('A user connected');
+
+   // Send a message after a timeout of 4 seconds defalult events
+    // setTimeout(function() {
+    // socket.send('Sent a message 4seconds after connection!');
+    // }, 4000);
 
 
-// io.on('connection', function(socket){
-//     socket.on('chat message', function(msg){
-//       console.log('message: ' + msg);
-//     });
-//   });
+  //Send a message to client when custom events 
+//    setTimeout(function() {
+//       //Sending an object when emmiting an event
+//       socket.emit('testerEvent', { description: 'A custom event named testerEvent!'});
+//    }, 4000);
 
-io.on('connection', function (socket) {
-    socket.broadcast.emit('hi');
-});
-
-io.on('connection', function (socket) {
-    socket.on('chat message', function (msg) {
-        io.emit('chat message', msg);
+io.on('connection', function(socket) {
+    socket.on('clientEvent', function(data) {
+       console.log(data);
     });
+ });
+ 
+ 
+   //Whenever someone disconnects this piece of code executed
+   socket.on('disconnect', function () {
+      console.log('A user disconnected');
+   });
 });
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+http.listen(3000, function() {
+   console.log('listening on *:3000');
 });
