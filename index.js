b45a47596907de5620cb890bdcf341e7f40e662c
unknown <newmohib@gmail.com>
2020-03-05 18:03:49 +0600

    connected new then increase one

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ app.get('/', function(req, res) {
    res.sendFile(__dirname + '/index.html');
 });
 
-var nsp = io.of('/my-namespace');
-nsp.on('connection', function(socket) {
-   console.log('someone connected');
-   nsp.emit('welcome', 'Hello everyone! ');
-});
+var roomno = 1;
+io.on('connection', function(socket) {
+   //Increase roomno 2 clients are present in a room.
+   if(io.nsps['/'].adapter.rooms["room-"+roomno] && io.nsps['/'].adapter.rooms["room-"+roomno].length > 1) roomno++;
+   socket.join("room-"+roomno);
+   
+   //Send this event to everyone in the room.
+   io.sockets.in("room-"+roomno).emit('connectToRoom', "You are in room no. "+roomno);
+})
+
 
 http.listen(3000, function() {
    console.log('listening on *:3000');
