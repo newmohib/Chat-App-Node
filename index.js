5926e415fb6da8cf3df4fbda78513ccf874990ff
unknown <newmohib@gmail.com>
2020-03-05 16:10:14 +0600

    custom namespace with of function

--- index.js
+++ index.js
@@ -3,18 +3,13 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-   res.sendfile('index.html');
+   res.sendFile(__dirname + '/index.html');
 });
 
-var clients = 0;
-io.on('connection', function(socket) {
-   clients++;
-   socket.emit('newclientconnect',{ description: 'Hey, welcome!'});
-   socket.broadcast.emit('newclientconnect',{ description: clients + ' clients connected!'})
-   socket.on('disconnect', function () {
-      clients--;
-      socket.broadcast.emit('newclientconnect',{ description: clients + ' clients connected!'})
-   });
+var nsp = io.of('/my-namespace');
+nsp.on('connection', function(socket) {
+   console.log('someone connected');
+   nsp.emit('welcome', 'Hello everyone! ');
 });
 
 http.listen(3000, function() {
